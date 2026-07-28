a1a3c0114e2477d45588aa69256782c49a733829
Vishal <usp.vishal@gmail.com>
2026-07-28 12:02:09 +0530

    changes

---
+++ littlechef/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Littlechef-1.0";
+const contentToCache = [
+    "Build/littlechef.loader.js",
+    "Build/littlechef.framework.js.unityweb",
+    "Build/littlechef.data.unityweb",
+    "Build/littlechef.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
